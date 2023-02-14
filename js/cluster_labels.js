92ca5cac0dbcf42ac66ee78410c0d5c991928133
Carmine Ferrara <125364778+SesaFairnessPracticesCatalog@users.noreply.github.com>
2023-02-14 12:45:50 +0100

    Starting commit

---
+++ js/cluster_labels.js
@@ -0,0 +1,37 @@
+
+    $(document).ready(function(){
+        $(".i1").text("Similar Average and Very High Impact");
+    });
+
+    $(document).ready(function(){
+        $(".i2").text("Average-High Impact");
+    });
+
+    $(document).ready(function(){
+        $(".i3").text("High Impact");
+    });
+
+
+    $(document).ready(function(){
+        $(".f1").text("Similar Average and Very High Frequency");
+    });
+
+    $(document).ready(function(){
+        $(".f2").text("Neutral Frequency distribution");
+    });
+
+    $(document).ready(function(){
+        $(".f3").text("Average frequency");
+    });
+
+    $(document).ready(function(){
+        $(".e1").text("Neutral effort");
+    });
+
+    $(document).ready(function(){
+        $(".e2").text("High or Very High Effort");
+    });
+
+    $(document).ready(function(){
+        $(".e3").text("Average High effort");
+    });
